ee4fd91bad23452eb8f8a7a385b5491909bd29e4
efrat91615 <efrat91615@gmail.com>
2026-05-27 19:57:49 +0300

    first commit

---
+++ WebApiShop-master/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using System.Threading.Tasks;
+namespace Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        string filePath = "..\\WebApiShop\\FileUsers.txt";
+
+        MyShop_216128025Context _myShop_216128025Context;
+
+        public UserRepository(MyShop_216128025Context myShop_216128025Context)
+        {
+            _myShop_216128025Context = myShop_216128025Context;
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            var user = await _myShop_216128025Context.Users.FindAsync(id);
+            return user;
+        }
+        public async Task<User> AddUser(User user)
+        {
+            await _myShop_216128025Context.Users.AddAsync(user);
+            await _myShop_216128025Context.SaveChangesAsync();
+            return user;
+        }
+        public async Task<User> Login(User loginUser)
+        {
+            var user = await _myShop_216128025Context.Users.FirstOrDefaultAsync(x=>x.UserName == loginUser.UserName &&
+            x.Password== loginUser.Password);
+            return user;
+
+        }
+        public async void UpdateUser(int id, User user)
+        {
+            _myShop_216128025Context.Users.Update(user);
+            await _myShop_216128025Context.SaveChangesAsync();
+
+        }
+
+
+        public async Task<IEnumerable<User>> GetAsync()
+        {
+            return await _myShop_216128025Context.Users.ToListAsync(); ;
+        }
+    }
+
+}
---
+++ WebApiShop-master/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using DTOs;
+using Entities;
+using Repositories;
+using System.Text.Json;
+namespace Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;// = new UserRepository();
+        private readonly IMapper _mapper;
+        PasswordService password = new PasswordService();
+
+        public UserService(IUserRepository repository,IMapper mapper)
+        {
+            _repository = repository;
+            _mapper=mapper;
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            return await _repository.GetUserById(id);
+        }
+        public async Task<User> AddUser(User user)
+        {
+            if (password.CheckPassword(user.Password).Level < 3)
+                return null;
+            return await _repository.AddUser(user);
+        }
+        public async Task<User> Login(User user)
+        {
+            return await _repository.Login(user);
+        }
+        public void UpdateUser(int id, User user)
+        {
+            _repository.UpdateUser(id, user);
+        }
+
+
+        public async Task<IEnumerable<UserDTO>> GetAsync()
+        {
+            IEnumerable<User> users = await _repository.GetAsync();
+            IEnumerable<UserDTO> usersDTO = _mapper.Map<IEnumerable<User>, IEnumerable<UserDTO>>(users);
+            return usersDTO;
+        }
+
+
+
+
+    }
+}
